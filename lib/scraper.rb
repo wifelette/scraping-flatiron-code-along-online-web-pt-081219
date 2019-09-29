4657e3018ed5586c91dabbe50c7d2b94fb343594
wifelette <leahsilber@gmail.com>
2019-09-28 20:54:46 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,31 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry-byebug'
 
 require_relative './course.rb'
 
 class Scraper
+  def initialize
+    get_page
+  end 
+  
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    # @doc.css(".post")
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
   
   def print_courses
     self.make_courses
